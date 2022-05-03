41c2198d7595753be9e59ac1a5c52c857db38aff
GianluigiEpifani <101205771+GianluigiEpifani@users.noreply.github.com>
2022-05-03 20:39:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+
+using namespace std;
 
 int main(){
+   int d=0;
+     cin>>d;
+     float valori[d];
+     for (int i=0; i<d; i++){
+         cin>>valori[i];
+     }
+     float x= funzione(valori, d);
+     cout<<x<<endl;
   return 0;
 }
